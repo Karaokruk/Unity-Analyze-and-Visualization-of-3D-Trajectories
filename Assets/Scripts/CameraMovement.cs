7088f9da5b887807e16edb395e9fcf0eda800f2c
Kruk <karaokruk@hotmail.fr>
2021-02-12 14:55:52 +0100

    Camera script

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    [Range(0.0f, 1.0f)]
+    private float sensitivity = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xAxisValue = Input.GetAxis("Horizontal");
+        float zAxisValue = Input.GetAxis("Vertical");
+        if (Camera.current != null) Camera.current.transform.Translate(new Vector3(xAxisValue, 0.0f, zAxisValue) * sensitivity);
+    }
+}
